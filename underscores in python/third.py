90998886e4d6edbead3ea49563bbbdba183d4cfb
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-12-24 23:36:56 +0330

    create "underscores in python"

---
+++ underscores in python/third.py
@@ -0,0 +1,10 @@
+from first import _b
+
+_b()
+
+# when to run third.py
+
+# $ "C:/Users/Ahmad Ali/AppData/Local/Programs/Python/Python39/python.exe" "c:/Users/Ahmad Ali/Desktop/project/Important-points-of-Python-and-Django-and-etc/underscores in python/third.py"
+# A function 
+# B function
+# B function

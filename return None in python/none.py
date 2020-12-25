0acf68c19191a30218b55aed362db155d72bd7fa
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-12-25 08:35:49 +0330

    create "return None in python"

---
+++ return None in python/none.py
@@ -0,0 +1,31 @@
+
+
+def show1(value):
+    if value:
+        return value
+    else:
+        return None
+
+
+def show2(value):
+    if value:
+        return value
+    else:
+        return 
+
+
+def show3(value):
+    if value:
+        return value
+
+
+
+print(type(show1(1)))
+print(type(show2(1)))
+print(type(show3(1)))
+
+print(type(show1(0)))
+print(type(show2(0)))
+print(type(show3(0)))
+
+    

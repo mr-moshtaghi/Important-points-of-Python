a1b843a41717dc8cf50cf2a80cb08343e740d46f
mr-moshtaghi <mr.ahmadalimoshtaghi@gmail.com>
2020-12-25 08:01:20 +0330

    create "Difference between __str__ and __repr__ in python"

---
+++ Difference between __str__ and __repr__ in python/__str__ vs __repr__.py
@@ -0,0 +1,23 @@
+
+
+class Person:
+    def __init__(self, name , age):
+        self.name = name
+        self.age = age
+
+
+    def __str__ (self):
+        return f'{self.name} is {self.age} years old'
+
+    def __repr__(self):
+        # return f'Person({self.name} , {self.age})'
+        return f'{self.__class__.__name__}({self.name} , {self.age})'
+    
+
+P = Person('amir' , 21)
+
+print(P)
+
+print(str(P))
+
+print(repr(P))

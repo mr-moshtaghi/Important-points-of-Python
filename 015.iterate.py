f1314d684bcb0fbcbe2fedf51dbaef2e8e06877a
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2021-12-03 00:17:33 +0330

    update

---
+++ 015.iterate.py
@@ -0,0 +1,21 @@
+'''
+    Itrerable, Iterator, Iteration => Iterate
+'''
+
+for i in range(1, 11):
+    print(i)
+
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+for num in nums:
+    print(num)
+
+
+# iterator => next()  or __next__()
+
+inums = iter(nums)
+
+print(next(inums))
+print(next(inums))
+print(next(inums))
+print(inums.__next__())

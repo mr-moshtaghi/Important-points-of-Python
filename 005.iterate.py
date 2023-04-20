aa8efcd5963eb559a4584036ff162f0c0cf1b31a
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-04-20 17:08:13 +0330

    update

--- 005.iterate.py
+++ 005.iterate.py
@@ -1,6 +1,6 @@
-'''
+"""
     Itrerable, Iterator, Iteration => Iterate
-'''
+"""
 
 for i in range(1, 11):
     print(i)
--- 007.generator.py
+++ 007.generator.py
@@ -1,49 +1,54 @@
-'''
+"""
 	Generator => 1.function, 2.experssion(comprehension)
-	
+
 	Lazy Evaluation
-'''
+"""
+
 
 def show1():
-	return "show..."
-	
+    return "show..."
+
+
 def show2():
-	yield "show..."
-	yield "Hello......"
-	
+    yield "show..."
+    yield "Hello......"
+
+
 print("show1 = ", show1())
 print("show2 = ", show2())
 
 for i in show2():
-	print(i)
-	
+    print(i)
+
 print("#############################")
 
-#-----------------------------------------
+
+# -----------------------------------------
 
 
 def show3(num):
-	print("starting....")
-	while num > 0:
-		yield num
-		num = num - 1
-		
-		
+    print("starting....")
+    while num > 0:
+        yield num
+        num = num - 1
+
+
 print(show3(5))
 
 for i in show3(5):
-	print(i)
-	
+    print(i)
+
 print("##############################")
 
-#-----------------------------------------
+
+# -----------------------------------------
 def show4(num):
-	print("starting....")
-	while num > 0:
-		yield num
-		num = num - 1
-		
-		
+    print("starting....")
+    while num > 0:
+        yield num
+        num = num - 1
+
+
 s = show4(5)
 
 print(next(s))
@@ -54,14 +59,15 @@ print(next(s))
 
 print("##############################")
 
-#-----------------------------------------
+# -----------------------------------------
 
 
-even_nums1 = [x for x in range(11) if x%2 == 0] # list comprehension
+even_nums1 = [x for x in range(11) if x % 2 == 0]  # list comprehension
 
-even_nums2 = (x for x in range(11) if x%2 == 0) # generator expression(comprehension)
+even_nums2 = (x for x in range(11) if x % 2 == 0)  # generator expression(comprehension)
 
 print(even_nums1)
+# print(list(even_nums2))
 print(even_nums2)
 
 print(next(even_nums2))
@@ -71,27 +77,4 @@ print(next(even_nums2))
 
 print("##############################")
 
-#-----------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
+# -----------------------------------------
